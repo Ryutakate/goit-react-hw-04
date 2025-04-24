fb3c128a65d691d3a8f88564de17408a9db4161e
Ryutakate <magegavladislav@gmail.com>
2025-04-25 02:12:57 +0300

    add src components and app.jsx v-1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' 
-    ? '/goit-react-hw-04/'  // Для GitHub Pages
-    : '/',                  // Для dev і Vercel
-})
+  base: '/',  
+  json: {
+    stringify: true,
+  },
+});
+
